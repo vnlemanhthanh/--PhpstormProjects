7fc0c724b2091b9bcfcc6a44faee1b1cd72c4bdb
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-11 09:06:54 +0700

    hello_world.php

---
+++ php-for-beginners/www/hello_world.php
@@ -0,0 +1,23 @@
+<?php
+$string = "Hello";
+$count = 10;
+$size = 2;
+$bool = false;
+$int = 10;
+$null = null;
+
+var_dump($string . "<hr/>");
+var_dump($count + 5);
+var_dump($count * $size);
+var_dump(!$bool and null);
+var_dump($int);
+var_dump($null);
+var_dump(null);
+
+//$message = "Hello World";
+//$count = 3;
+//$price = 1.99;
+//
+//var_dump($message);
+//var_dump($count);
+//var_dump($price);
