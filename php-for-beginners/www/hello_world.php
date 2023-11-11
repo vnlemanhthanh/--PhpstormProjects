2098f14ed284f0cc7bc8749289f36f806f8e1017
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-11 09:38:49 +0700

    arrays

--- php-for-beginners/www/hello_world.php
+++ php-for-beginners/www/hello_world.php
@@ -1,18 +1,51 @@
 <?php
-$string = "Hello";
-$count = 10;
-$size = 2;
-$bool = false;
-$int = 10;
-$null = null;
 
-var_dump($string . "<hr/>");
-var_dump($count + 5);
-var_dump($count * $size);
-var_dump(!$bool and null);
-var_dump($int);
-var_dump($null);
-var_dump(null);
+$articles = [
+    ["title" => "First post", "content" => "This is the first post"],
+    ["title" => "Another post", "content" => 'another post to read here'],
+    ['title' => 'Read this', 'content' => 'You must read this article']
+];
+
+
+
+var_dump($articles);
+echo "<br/><br>";
+var_dump($articles[0]['content']);
+echo "<hr/>";
+
+//$articles1 = 1.34;
+//
+//$articles = [
+//    "first" => $articles1,
+//    "second" => "Another post",
+//    "four" => "Read this!"
+//];
+//
+//var_dump($articles);
+//var_dump($articles["four"][1]);
+
+//$articles = ["First post", "Second Post", "Read this!"];
+//
+////echo $articles;
+//var_dump($articles);
+//echo "<hr/>";
+////var_dump("<hr/>");
+//var_dump($articles[0]);
+
+//$string = "Hello";
+//$count = 10;
+//$size = 2;
+//$bool = false;
+//$int = 10;
+//$null = null;
+//
+//var_dump($string . "<hr/>");
+//var_dump($count + 5);
+//var_dump($count * $size);
+//var_dump(!$bool and null);
+//var_dump($int);
+//var_dump($null);
+//var_dump(null);
 
 //$message = "Hello World";
 //$count = 3;
