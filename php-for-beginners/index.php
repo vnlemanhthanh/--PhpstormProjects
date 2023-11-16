99150c6a6895c5934b889b5b8c0ee04f8bd6c5e5
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-16 15:42:46 +0700

    Combine PHP and HTML to Show a Formatted List of Articles

--- php-for-beginners/index.php
+++ php-for-beginners/index.php
@@ -1,28 +1,60 @@
 <?php
-$hour = 19;
+
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "cms_www";
+$db_pass = "0Cxuwi27jEViUxd3";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
+
+$sql = "SELECT *
+        FROM article
+        ORDER BY published_at;";
+
+$results = mysqli_query($conn, $sql);
+
+if ($results === false) {
+    echo mysqli_error($conn);
+} else {
+    $articles = mysqli_fetch_all($results, MYSQLI_ASSOC);
+}
+
 ?>
 
-<!doctype html>
-<html lang="en">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>My blog</title>
+    <meta charset="utf-8">
 </head>
 <body>
-    <h1>Lorem Ipsum</h1>
 
-    <?php if ($hour < 12): ?>
-        Good morning
-    <?php elseif ($hour < 18): ?>
-        Good afternoon
-    <?php elseif ($hour < 22): ?>
-        Good evening
+<header>
+    <h1>My blog</h1>
+</header>
+
+<main>
+    <?php if (empty($articles)): ?>
+        <p>No articles found.</p>
     <?php else: ?>
-        Good night
-    <?php endif; ?>
 
+        <ul>
+            <?php foreach ($articles as $article): ?>
+                <li>
+                    <article>
+                        <h2><?= $article['title']; ?></h2>
+                        <p><?= $article['content']; ?></p>
+                    </article>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+
+    <?php endif; ?>
+</main>
 </body>
 </html>
