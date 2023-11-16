587b2486c7cd9e4ce9cf2522f3b194833f27099e
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-16 13:13:57 +0700

    switch

--- php-for-beginners/www/control.php
+++ php-for-beginners/www/control.php
@@ -1,6 +1,29 @@
 <?php
 
 echo "<hr/>";
+$day = "Wed";
+
+switch ($day) {
+    case 'Mon':
+        echo "Monday";
+        break;
+    case 'Tue':
+        echo "Tuesday"; break;
+    case 'Wed':
+        echo " Wednesday"; break;
+    case 'Thu':
+        echo "Thursday"; break;
+    case 'Fri':
+        echo "Friday"; break;
+    case 'Sat':
+        echo "Saturday"; break;
+    case 'Sun':
+        echo "Sunday"; break;
+    default:
+        echo "Days on Sunday";
+}
+
+echo "<hr/>";
 $hours = 17;
 
 if ($hours < 12) {
